aa376a5a8bee0878ad33e46707a293bb28a3bcb5
Natthakit Khamso <natthakit.khamso@gmail.com>
2015-10-31 16:30:56 +0100

    Changed: Namespace to "YoungChampion"

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace YoungChampion\Models;
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace YoungChampion\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
